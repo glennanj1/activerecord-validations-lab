5b54dfabe8b4a04a8219d8e3d7141e1168126a41
github username <github email address>
2021-03-04 16:29:21 -0500

    commit tree f43bead7407dd8de4f7674ea11329df30c66912c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_bait
+    
+    def title_bait
+        bait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        if title.present? && !bait.any? { |x| self.title.include? x }
+        errors.add(:title, "is this working")
+        end
+    end
 end
